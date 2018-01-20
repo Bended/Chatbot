903b37f3262abfee0d560d3c67e6e79e82c0f0bf
Benny Deddouch <b.deddouch@gmail.com>
2018-01-20 22:03:52 +0200

    final

--- boto.py
+++ boto.py
@@ -133,8 +133,8 @@ def chat():
 #######
 
 def main():
-    #run(host='localhost', port=7002)
-    run(host='0.0.0.0', port=argv[1])
+    run(host='localhost', port=7000)
+    #run(host='0.0.0.0', port=argv[1])
 
 if __name__ == '__main__':
     main()
